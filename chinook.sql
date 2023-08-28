0696c70bd468f28bc176f6904b40d751bafbbb61
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-28 09:00:11 +0100

    2023.08.28: Column returns

--- chinook.sql
+++ chinook.sql
@@ -11,3 +11,15 @@ FROM artisits;
 -- Find all data from the invoices table
 SELECT *
 FROM invoices;
+
+-- Return Playlist ID and Name from the playlist table
+SELECT PlaylistId, Name
+FROM playlist;
+
+-- Return Customer ID, Invoice Date, and Billing City from the Invoices table
+SELECT CustomerId, InvoiceDate, BillingCity
+FROM invoices;
+
+-- Return First Name, Last Name, Email, and Phone Number from the Customer table
+SELECT FirstName, LastName, email, phone
+FROM customer;
