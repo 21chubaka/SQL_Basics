c0614137f7818de53554343a622266317885711a
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-28 08:52:38 +0100

    2023.08.28: Select alls

--- chinook.sql
+++ chinook.sql
@@ -3,3 +3,11 @@
 -- Find all data from the tracks table
 SELECT *
 FROM tracks;
+
+-- Find all data from the artists table
+SELECT *
+FROM artisits;
+
+-- Find all data from the invoices table
+SELECT *
+FROM invoices;
