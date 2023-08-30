57b89150bbd980c775b922952a30b78b9cdacb10
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-08-30 09:35:27 +0100

    2023.08.30: Refactoring

--- chinook.sql
+++ chinook.sql
@@ -74,7 +74,7 @@ SELECT email
 FROM customer
 WHERE email LIKE 'J%@gmail.com';
 
--- Order By and Group By
+-- Order By, Group By, Having
 -- Find all the invoices with billing city set as Brasilia, Edmonton, and Vancouver and
 -- sort the results in descending order by Invoice ID
 SELECT InvoiceId, BillingCity, total
