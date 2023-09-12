fc962f85061548e531ec4b123e8f2652af0b7909
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-09-12 17:28:19 +0100

    2023.09.12: Exercise 2 query

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -2,3 +2,9 @@
 SELECT email_address
 FROM dsv1069.users
 WHERE deleted_at IS NULL;
+
+-- Return the number of items for sale for each category from the items table
+SELECT category, COUNT(*) AS num_of_items
+FROM dsv1069.items
+GROUP BY category
+ORDER BY category;
