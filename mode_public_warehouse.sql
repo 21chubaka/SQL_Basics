ccf2047f1834ac177cf7dce5272728b97ee78475
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-09-21 14:06:42 +0100

    2023.09.21: Flex data format, exer 3

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -131,3 +131,18 @@ SELECT event_id, event_time, user_id, platform,
 FROM dsv1069.events 
 WHERE event_name = 'view_item'
 ORDER BY event_id;
+
+-- Exercise 3:
+SELECT event_id, event_time, user_id, platform,
+        MAX(CASE WHEN parameter_name = 'item_id' THEN
+              CAST (parameter_value AS INT) ELSE
+              NULL 
+              END) AS item_id,
+        MAX(CASE WHEN parameter_name = 'referrer' THEN
+              parameter_value ELSE
+              NULL 
+              END) AS referrer
+FROM dsv1069.events 
+WHERE event_name = 'view_item'
+GROUP BY event_id, event_time, user_id, platform
+ORDER BY event_id;
