e2adbee6ac11e33ce1102c69ec71eb1a7bbbf93f
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-09-30 12:05:15 +0100

    2023.09.30: Clean query wrksht, exer 3

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -298,3 +298,20 @@ FROM view_item_events_1
 LIMIT 10;
 
 DROP TABLE view_item_events_1;
+
+-- Exercise 3:
+CREATE TABLE view_item_events_1 AS 
+  SELECT event_id, 
+          TIMESTAMP(event_time) AS event_time, 
+          user_id, platform,
+          MAX(CASE WHEN parameter_name = 'item_id'
+                      THEN parameter_value
+                          ELSE NULL 
+                          END) AS item_id,
+          MAX(CASE WHEN parameter_name = 'referrer'
+                      THEN parameter_value
+                          ELSE NULL 
+                          END) AS referrer
+  FROM dsv1069.events 
+  WHERE event_name = 'view_item'
+  GROUP BY event_id, event_time, user_id, platform;
