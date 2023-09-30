26a96977d7db3d7c6e98057c783260af6ec0ecbc
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-09-30 12:08:15 +0100

    2023.09.30: Clean query wrksht, exer 6

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -351,3 +351,20 @@ INSERT INTO 'view_item_events_1'
   FROM dsv1069.events 
   WHERE event_name = 'view_item'
   GROUP BY event_id, event_time, user_id, platform;
+
+-- Exercise 6:
+REPLACE INTO 'view_item_events_1'
+  SELECT event_id, 
+          TIMESTAMP(event_time) AS event_time, 
+          user_id, platform,
+          MAX(CASE WHEN parameter_name = 'item_id'
+                      THEN parameter_value
+                          ELSE NULL 
+                          END) AS item_id,
+          MAX(CASE WHEN parameter_name = 'referrer'
+                      THEN parameter_value
+                          ELSE NULL 
+                          END) AS referrer
+  FROM dsv1069.events 
+  WHERE event_name = 'view_item'
+  GROUP BY event_id, event_time, user_id, platform;
