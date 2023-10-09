285ac91217d107daf5f5c1398009c811d83b61bf
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-09 16:57:44 +0100

    2023.10.09: Promo email, exer 3

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -545,3 +545,22 @@ JOIN dsv1069.users
   ON users.id = recent_views.user_id 
 JOIN dsv1069.items 
   ON items.id = recent_views.item_id;
+
+/* Exercise 3:
+-- With Columns -- */
+SELECT users.id AS user_id, users.email_address AS email,
+      items.id AS item_id,
+      items.name AS item_name,
+      items.category AS items_cat
+FROM (
+  SELECT user_id, item_id, event_time,
+          ROW_NUMBER() OVER (
+            PARTITION BY user_id ORDER BY event_time DESC
+            ) AS view_number
+  FROM dsv1069.view_item_events
+  ) recent_views
+JOIN dsv1069.users
+  ON users.id = recent_views.user_id 
+JOIN dsv1069.items 
+  ON items.id = recent_views.item_id 
+WHERE view_number = 1;
