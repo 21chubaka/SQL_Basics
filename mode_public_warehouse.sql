4fd4c7c516a1e4700484b9d931cd8ea5338105be
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-10 19:56:18 +0100

    2023.10.10: Test assign wrksht, exer 3

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -692,3 +692,19 @@ FROM dsv1069.events
 WHERE event_name = 'test_assignment' AND
       parameter_name = 'test_id'
 GROUP BY parameter_value, DATE(event_time);
+
+-- Exercise 3:
+-- Assignments Event Table
+SELECT event_id, event_time, user_id, platform,
+        MAX(CASE WHEN parameter_name = 'test_id'
+                  THEN CAST(parameter_value AS INT)
+                  ELSE NULL 
+                  END) AS test_id,
+        MAX(CASE WHEN parameter_name = 'test_assignment'
+                  THEN parameter_value
+                  ELSE NULL 
+                  END) AS test_assignment
+FROM dsv1069.events 
+WHERE event_name = 'test_assignment'
+GROUP BY event_id, event_time, user_id, platform
+ORDER BY event_id;
