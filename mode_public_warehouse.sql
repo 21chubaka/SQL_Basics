f1bf5e8c527133e3e1bb909638e13ffba2951b92
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-10 19:57:16 +0100

    2023.10.10: Test assign wrksht, exer 4

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -708,3 +708,26 @@ FROM dsv1069.events
 WHERE event_name = 'test_assignment'
 GROUP BY event_id, event_time, user_id, platform
 ORDER BY event_id;
+
+-- Exercise 4:
+-- Check Assignments
+SELECT user_id,
+        COUNT(DISTINCT(test_assignment)) AS assignments
+FROM(
+  SELECT event_id, event_time, user_id, platform,
+          MAX(CASE WHEN parameter_name = 'test_id'
+                    THEN CAST(parameter_value AS INT)
+                    ELSE NULL 
+                    END) AS test_id,
+          MAX(CASE WHEN parameter_name = 'test_assignment'
+                    THEN parameter_value
+                    ELSE NULL 
+                    END) AS test_assignment
+  FROM dsv1069.events 
+  WHERE event_name = 'test_assignment'
+  GROUP BY event_id, event_time, user_id, platform
+  ORDER BY event_id
+  ) test_events
+WHERE test_id = 5
+GROUP BY user_id
+ORDER BY assignments DESC;
