6672635f8de0a37d86325ec80fed6413d2771b3a
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-11 12:41:51 +0100

    2023.10.11: Test Metric wrksht, exer 1

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -731,3 +731,29 @@ FROM(
 WHERE test_id = 5
 GROUP BY user_id
 ORDER BY assignments DESC;
+
+-- 10. Create a Test Metric Worksheet
+-- Exercise 1:
+-- Compute Order Binary
+SELECT test_events.test_id, test_events.test_assignment, test_events.user_id,
+        MAX(CASE WHEN orders.created_at > test_events.event_time 
+              THEN 1 
+              ELSE 0 
+              END) AS orders_after_assign_binary
+FROM (
+  SELECT event_id, event_time, user_id, platform,
+          MAX(CASE WHEN parameter_name = 'test_id'
+                    THEN CAST(parameter_value AS INT)
+                    ELSE NULL 
+                    END) AS test_id,
+          MAX(CASE WHEN parameter_name = 'test_assignment'
+                    THEN parameter_value
+                    ELSE NULL 
+                    END) AS test_assignment
+  FROM dsv1069.events 
+  WHERE event_name = 'test_assignment'
+  GROUP BY event_id, event_time, user_id, platform
+  ) test_events
+LEFT JOIN dsv1069.orders ON 
+          orders.user_id = test_events.user_id
+GROUP BY test_events.test_id, test_events.test_assignment, test_events.user_id;
