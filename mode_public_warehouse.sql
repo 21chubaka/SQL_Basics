98582545ee696a034817a311e24299b55671324d
21chubaka <92584853+21chubaka@users.noreply.github.com>
2023-10-11 12:45:04 +0100

    2023.10.11: Test Metric wrksht, exer 2

--- mode_public_warehouse.sql
+++ mode_public_warehouse.sql
@@ -757,3 +757,36 @@ FROM (
 LEFT JOIN dsv1069.orders ON 
           orders.user_id = test_events.user_id
 GROUP BY test_events.test_id, test_events.test_assignment, test_events.user_id;
+
+-- Exercise 2:
+-- Compute Mean Metrics
+SELECT test_events.test_id, test_events.test_assignment, test_events.user_id,
+        COUNT(DISTINCT(CASE WHEN orders.created_at > test_events.event_time 
+              THEN invoice_id 
+              ELSE NULL
+              END)) AS orders_after_assign,
+        COUNT(DISTINCT(CASE WHEN orders.created_at > test_events.event_time 
+              THEN line_item_id 
+              ELSE NULL
+              END)) AS items_after_assign,
+        SUM(CASE WHEN orders.created_at > test_events.event_time 
+              THEN price
+              ELSE 0
+              END) AS total_rev
+FROM (
+  SELECT event_id, event_time, user_id, platform,
+          MAX(CASE WHEN parameter_name = 'test_id'
+                    THEN CAST(parameter_value AS INT)
+                    ELSE NULL 
+                    END) AS test_id,
+          MAX(CASE WHEN parameter_name = 'test_assignment'
+                    THEN parameter_value
+                    ELSE NULL 
+                    END) AS test_assignment
+  FROM dsv1069.events 
+  WHERE event_name = 'test_assignment'
+  GROUP BY event_id, event_time, user_id, platform
+  ) test_events
+LEFT JOIN dsv1069.orders ON 
+          orders.user_id = test_events.user_id
+GROUP BY test_events.test_id, test_events.test_assignment, test_events.user_id;
